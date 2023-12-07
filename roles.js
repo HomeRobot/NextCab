df0380729b82f430f9bfb1e25cc261888ad9dcdf
Captain <alex.bakulin@gmail.com>
2023-12-07 13:24:22 +0100

    sync

--- roles.js
+++ roles.js
@@ -1,19 +1,19 @@
 const roles = {
-    'admin': [
-        { 'action': ['read', 'update', 'changeState', 'delete', 'create', 'getList'], 'resource': 'users' },
-        { 'action': ['read', 'update', 'changeState', 'delete', 'create', 'getList'], 'resource': 'clients' },
-        { 'action': ['read', 'update', 'changeState', 'delete', 'create', 'getList'], 'resource': 'bots' },
-        { 'action': ['read', 'update', 'changeState', 'delete', 'create', 'getList'], 'resource': 'pairs' },
-        { 'action': ['read', 'update', 'changeState', 'delete', 'create', 'getList'], 'resource': 'departments' },
+    "admin": [
+        { "action": ["read", "update", "changeState", "delete", "create", "getList"], "resource": "users" },
+        { "action": ["read", "update", "changeState", "delete", "create", "getList"], "resource": "clients" },
+        { "action": ["read", "update", "changeState", "delete", "create", "getList"], "resource": "bots" },
+        { "action": ["read", "update", "changeState", "delete", "create", "getList"], "resource": "pairs" },
+        { "action": ["read", "update", "changeState", "delete", "create", "getList"], "resource": "departments" },
     ],
-    'franchisee': [
-        { 'action': ['read', 'update', 'changeState'], 'resource': 'clients' },
-        { 'action': ['read', 'update', 'changeState', 'create', 'getList'], 'resource': 'bots' },
-        { 'action': ['read', 'update', 'changeState', 'create', 'getList'], 'resource': 'pairs' },
+    "manager": [
+        { "action": ["read", "update", "changeState"], "resource": "clients" },
+        { "action": ["read", "update", "changeState", "create", "getList"], "resource": "bots" },
+        { "action": ["read", "update", "changeState", "create", "getList"], "resource": "pairs" },
     ],
-    'client': [
-        { 'action': ['read', 'update', 'changeState', 'getList'], 'resource': 'bots' },
-        { 'action': ['read', 'update', 'changeState', 'create', 'getList'], 'resource': 'pairs' },
+    "client": [
+        { "action": ["read", "update", "changeState", "getList"], "resource": "bots" },
+        { "action": ["read", "update", "changeState", "create", "getList"], "resource": "pairs" },
     ]
 }
 
