a03d88fb1b84a9eda0e6990e33b7ea3e5e5fb6d6
Ivan <1vanguard@mail.ru>
2025-09-23 11:42:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,34 @@
 'use strict';
 
+const testDB = {
+    DB_DATABASE: "citonex_cnnct",
+    DB_USERNAME: "citonex_master",
+    DB_PASSWORD: "GzkWKR5VsykdamOM",
+    DB_PREFIX: "eielu_",
+}
+
+const productionDB = {
+    DB_DATABASE: "citonex_algo",
+    DB_USERNAME: "citonex_algo",
+    DB_PASSWORD: "ChoyK6oZfbUPmkr4",
+    DB_PREFIX: "eielu_",
+}
+
+const currentDB = productionDB
+
+
+
 module.exports = {
     DB_HOST: "185.195.69.110",
-    DB_DATABASE: "citonex_cabinet",
-    DB_USERNAME: "citonex_sql",
-    DB_PASSWORD: "2b1WWzPVWD7FaeCk",
+    DB_DATABASE: currentDB.DB_DATABASE,
+    DB_USERNAME: currentDB.DB_USERNAME,
+    DB_PASSWORD: currentDB.DB_PASSWORD,
+    DB_PREFIX: currentDB.DB_PREFIX,
     secretKey: 'your_secret_key',
-    ALLOWED_ORIGINS: ['https://new.adminalgo.ae:5173', 'https://dev.adminalgo.ae:5174'],
-    SSL_KEY_PATH: '/home/citonex/conf/web/api.adminalgo.ae/ssl/api.adminalgo.ae.key',
-    SSL_CERT_PATH: '/home/citonex/conf/web/api.adminalgo.ae/ssl/api.adminalgo.ae.pem'
+    ALLOWED_ORIGINS: ['https://localhost:5173', 'https://192.168.1.11:5173'],
+    REDIS_HOST_01: '185.195.69.110',
+    REDIS_PORT_01: 6379,
+    REDIS_PASS_01: 'cS2PSNU3flzf42rsg',
+    SSL_KEY_PATH: '../private.key',
+    SSL_CERT_PATH: '../certificate.crt'
 }
